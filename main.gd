93ac1352e7c669f3f578e37ab8ee22ba33c9fc88
allanbts17 <allanbts.18music@gmail.com>
2022-04-20 17:12:32 -0500

    initial commit

---
+++ main.gd
@@ -0,0 +1,161 @@
+extends Node2D
+
+
+onready var snake_body = get_node("snakeBody")
+var snake_size
+var initial_position
+var initial_tail_position
+export (int) var speed_index #2
+var direction_list = []
+var distance_to_next_center = 2
+var distance_to_prev_center = 30
+var is_far_from_prev_center
+var reach_board_square_ind = [null,null]
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	snake_body.position = Global.board_offset
+	initial_position = Global.board_pos[Vector2(3,3)]
+	initial_tail_position = Global.board_pos[Vector2(2,3)]
+	var v = [2,3,5,6]
+	
+	#print('orginal: ',v)
+	#v.insert(1,10)
+	#print('with 10 on 1: ',v)
+
+	reset_snake_body()
+	
+	
+	#print("Count")
+	#print(snake_body.get_point_count())
+	#print(snake_body.points)
+	var snake_size = get_snake_size()
+	print('snake size: ',snake_size)
+	#print(Vector2(20,15).distance_to(Vector2(0,0)))
+
+
+func get_snake_size():
+	var size = 0
+	for i in range(snake_body.get_point_count()):
+		if i > 0:
+			size += snake_body.get_point_position(i).distance_to(snake_body.get_point_position(i-1))
+	return size
+
+func reset_snake_body():
+	snake_body.clear_points()
+	snake_body.add_point(initial_position)
+	#var t = Vector2(initial_position.x-1,initial_position.y)
+	#snake_body.add_point(t)
+	snake_body.add_point(initial_tail_position)
+	print('all points: ',snake_body.points)
+	print('point: ',snake_body.points[0])
+	
+func reach_point_to_board_square(point) -> bool:
+	var indx = round_place(((snake_body.points[point].x + Global.square_size.x/2)/Global.square_size.x)-1,2)
+	var indy = round_place(((snake_body.points[point].y + Global.square_size.y/2)/Global.square_size.y)-1,2)
+	var reached = false
+	if is_int(round_almost_int(indx,0.05)) and is_int(round_almost_int(indy,0.05)):
+		var index = Vector2(round_almost_int(indx,0.05),round_almost_int(indy,0.05))
+		if reach_board_square_ind[point] != index and reach_board_square_ind[point] != null:
+			reached = true
+		reach_board_square_ind[point] = index
+	return reached
+	
+func reach_board_square():
+	if reach_point_to_board_square(0):
+		Global.solaped_board_squares.push_back(reach_board_square_ind[0])
+		snake_body.points[0] = Global.board_pos[reach_board_square_ind[0]]
+				#is_far_from_prev_center = Global.board_pos[Global.solaped_board_squares[-1]].distance_to(snake_body.points[0])
+		if direction_list.size() > 0:# and is_far_from_prev_center > 0:#Global.solaped_board_squares[-1]:
+			Global.direction = direction_list[0]
+			direction_list.pop_front()
+			#ajustar direction de la serpiente
+			
+		#prints(index,snake_body.points[0])
+#reach_board_square_ind = index
+#Repetir lo mismo con snake_body.points[last] para remover con pop_front()
+	
+func change_velocity():
+	Global.velocity = Vector2()
+	Global.end_velocity = Vector2()
+	if Global.direction == "right":
+		Global.velocity.x += 1
+	if Global.direction == "left":
+		Global.velocity.x -= 1
+	if Global.direction == "down":
+		Global.velocity.y += 1
+	if Global.direction == "up":
+		Global.velocity.y -= 1
+		
+	if Global.velocity.length() > 0:
+		Global.velocity = Global.velocity.normalized() * Global.speed
+
+func is_int(num):
+	return num - int(num) == 0
+
+func round_place(num,places):
+	return (round(num*pow(10,places))/pow(10,places))
+	
+func round_almost_int(num,diference):
+	var int_part = int(num)
+	var dec_part = num - int_part
+	
+	if num <= int_part+diference:
+		return int_part
+	elif num >= int_part+1-diference:
+		return int_part+1
+	else:
+		return num
+
+func move_snake(_delta):
+	for i in snake_body.get_point_count():
+		snake_body.points[i] += Global.velocity * _delta
+
+func inputs():
+	if Input.is_action_just_pressed("ui_d"):
+		change_direction('right')
+	if Input.is_action_just_pressed("ui_a"):
+		change_direction('left')
+	if Input.is_action_just_pressed("ui_s"):
+		change_direction('down')
+	if Input.is_action_just_pressed("ui_w"):
+		change_direction('up')
+		
+func change_direction(new_direction):
+	if is_vertical(new_direction):
+		if direction_list.size() == 0:
+			if is_horizontal(Global.direction):
+				if direction_list.size() < 2:
+					direction_list.push_back(new_direction)
+		else:
+			if is_horizontal(direction_list[0]):
+				if direction_list.size() < 2:
+					direction_list.push_back(new_direction)
+	else:
+		if direction_list.size() == 0:
+			if is_vertical(Global.direction):
+				if direction_list.size() < 2:
+					direction_list.push_back(new_direction)
+		else:
+			if is_vertical(direction_list[0]):
+				if direction_list.size() < 2:
+					direction_list.push_back(new_direction)
+		#print(new_direction)
+		#print(direction_list)
+	
+	
+func is_vertical(direction):
+	return direction == 'up' or direction == 'down'
+	
+func is_horizontal(direction):
+	return direction == 'right' or direction == 'left'
+
+func _process(delta):
+	Global.speed = speed_index * 25
+	#print('delta: ',delta)
+	inputs()
+	change_velocity()
+	move_snake(delta)
+	reach_board_square()
+	
+	#print(Global.velocity * delta)
---
+++ main_game.gd
@@ -0,0 +1,16 @@
+extends Node
+
+
+var board_size = Vector2(645.75,687.45)
+var board_offset = Vector2(37,137)
+var board_squares = Vector2(15,16)
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
